31b073c77d816fe40323d93dc4525c7d441eae52
Robby Morgan <robby.d.morgan@gmail.com>
2023-10-10 12:16:33 -0500

    finished

--- bubble-sort.js
+++ bubble-sort.js
@@ -1,18 +1,33 @@
 
 function bubbleSort(arr) {
+  let swapped = true;
 
-    // Iterate through the array
+  while (swapped) {
+    swapped = false;
+    for (let i = 0; i < arr.length; i++){
+      if (arr[i] > arr[i + 1]) {
+        [arr[i], arr[i + 1]] = [arr[i + 1], arr[i]];
+        swapped = true;
+        console.log(arr.join(','));
+      }
+    }
+    
+  }
+  return arr;
+    
 
       // If the current value is greater than its neighbor to the right
         // Swap those values
 
         // Do not move this console.log
-        console.log(arr.join(","));
+        
 
     // If you get to the end of the array and no swaps have occurred, return
 
     // Otherwise, repeat from the beginning
 
 }
+
+bubbleSort([4, 2, 1, 3, 0]);
 
 module.exports = bubbleSort;
